e101cc5a1024527fcbceb1aba7ef29009ce08e7d
Hikari Luz <hikaridesuyoo@gmail.com>
2024-08-14 12:58:56 -0300

    fix: remove breaking lines

--- lua/evil-mode/init.lua
+++ lua/evil-mode/init.lua
@@ -1,31 +1,15 @@
 vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-
 local mateus
 
 mateus = {
   starting = 1,
   ending = 2,
   sort_pos = function(a, b)
-      if a[1] < b[1] then return {a, b} end
-      if a[1] > b[1] then return {b, a} end
-      if a[2] < b[2] then return {a, b} end
-      return {b, a}
+    if a[1] < b[1] then return { a, b } end
+    if a[1] > b[1] then return { b, a } end
+    if a[2] < b[2] then return { a, b } end
+    return { b, a }
   end,
 
   eq_pos = function(a, b) return a[1] == b[1] and a[2] == b[2] end,
@@ -37,34 +21,31 @@ mateus = {
 
       vim.api.nvim_feedkeys(keys_before, 'n', {})
       local pos_a = vim.fn.getcharpos('v')
-      pos_a = {pos_a[2], pos_a[3]}
+      pos_a = { pos_a[2], pos_a[3] }
 
       local pos_b = vim.fn.getcharpos('.')
-      pos_b = {pos_b[2], pos_b[3]}
-      
+      pos_b = { pos_b[2], pos_b[3] }
+
       local pos_start, pos_end = unpack(mateus.sort_pos(pos_a, pos_b))
-      local cursor = vim.api.nvim_win_get_cursor(0) 
-      local on_start = mateus.eq_pos(pos_start, {cursor[1], cursor[2] + 1}) 
-      local on_end = mateus.eq_pos(pos_end,  {cursor[1], cursor[2] + 1}) 
-      local already_there =  
-        (edge == mateus.starting and on_start) or
-	(edge == mateus.ending and on_end) 
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      local on_start = mateus.eq_pos(pos_start, { cursor[1], cursor[2] + 1 })
+      local on_end = mateus.eq_pos(pos_end, { cursor[1], cursor[2] + 1 })
+      local already_there =
+          (edge == mateus.starting and on_start) or
+          (edge == mateus.ending and on_end)
       if not already_there then
         vim.fn.feedkeys('o', 'n')
       end
-      
+
       vim.api.nvim_feedkeys(keys_after, 'n', {})
     end
   end
 }
 
-vim.opt.rtp:prepend(lazypath)
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 --
 vim.keymap.set('n', '<S-Up>', 'v<Up>')
 vim.keymap.set('n', '<S-Down>', 'v<Down>')
@@ -91,7 +72,7 @@ vim.keymap.set('i', '<S-Down>', '<Esc>v<Down>')
 vim.keymap.set('i', '<S-Left>', '<Esc>v')
 vim.keymap.set('i', '<S-Right>', '<Right><Esc>v')
 
-vim.keymap.set({'i', 'v'}, '<C-s>', '<Esc>:w<CR>i')
+vim.keymap.set({ 'i', 'v' }, '<C-s>', '<Esc>:w<CR>i')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 
 vim.keymap.set('n', '<C-v>', 'pi')
@@ -108,33 +89,8 @@ vim.keymap.set('n', '<Del>', 'i<Del>')
 
 vim.keymap.set('i', '<C-z>', '<Esc>ui')
 
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    "loctvl842/monokai-pro.nvim",
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-{
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-}
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "monokai-pro" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
-
-vim.cmd.colorscheme("monokai-pro")
 vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function(_e) 
+  callback = function(_e)
     if vim.opt.modifiable then
       vim.api.nvim_feedkeys("i", "n", {})
     end
@@ -144,8 +100,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 local builtin = require("telescope.builtin")
-vim.keymap.set({'i', 'v', 'n'}, '<C-p>', builtin.find_files)
-
-require("oil").setup()
+vim.keymap.set({ 'i', 'v', 'n' }, '<C-p>', builtin.find_files)
 vim.keymap.set('n', '<C-S-e>', "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set('i', '<C-S-e>', "<Esc><CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.print("a")
+return { setup = function(_opts) end }
